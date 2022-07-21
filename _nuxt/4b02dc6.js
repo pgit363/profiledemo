83a7fbf57bc1f0667126451f9ebde7643e741aed
pranav32 <pranav@indigitalit.com>
2022-07-21 17:31:08 +0530

    Updates

---
+++ _nuxt/4b02dc6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{420:function(n,t,e){"use strict";e.r(t);var r=e(251),c={data:function(){return{}}},l=e(64),component=Object(l.a)(c,(function(){return(0,this._self._c)(r.a,{attrs:{height:"100vh",src:"/images/bgcar.jpg"}})}),[],!1,null,null,null);t.default=component.exports}}]);
